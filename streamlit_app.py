e3872bf905e141d3c315a4e23a72f38641f91224
Benjamin Senst <benjamin.senst@gmail.com>
2023-12-02 01:21:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ end_time = time.time()
 
 if st.button("Perform L2 distance vector search"):
 
-    st.write(result.index(0))
+    st.write(type(result))
 
     # for hits in result:
     #     skip = [0]
